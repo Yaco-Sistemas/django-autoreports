0fd4de824b324a28ffae0896709c1875becef593
pmartin <pmartin@43d3a112-9a52-0410-bc29-92ccca371478>
2010-08-17 12:48:41 +0000

    Fixes #113 Now you can report to CSV or EXCEL

---
+++ autoreports/csv_to_excel.py
@@ -0,0 +1,61 @@
+# Based on http://sujitpal.blogspot.com/2007/02/python-script-to-convert-csv-files-to.html
+import csv
+from pyExcelerator import Workbook
+
+
+def openExcelSheet():
+    """ Opens a reference to an Excel WorkBook and Worksheet objects """
+    workbook = Workbook()
+    worksheet = workbook.add_sheet("Sheet 1")
+    return workbook, worksheet
+
+
+def validateOpts(response):
+    """ Returns option values specified, or the default if none """
+    titlePresent = False
+    linesPerFile = -1
+    sepChar = ","
+    return titlePresent, linesPerFile, sepChar
+
+
+def writeExcelHeader(worksheet, titleCols):
+    """ Write the header line into the worksheet """
+    cno = 0
+    for titleCol in titleCols:
+        worksheet.write(0, cno, titleCol)
+        cno = cno + 1
+
+
+def writeExcelRow(worksheet, lno, columns):
+    """ Write a non-header row into the worksheet """
+    cno = 0
+    for column in columns:
+        worksheet.write(lno, cno, column.decode('utf-8'))
+        cno = cno + 1
+
+
+def closeExcelSheet(response, workbook):
+    """ Saves the in-memory WorkBook object into the specified file """
+    response.content = workbook.get_biff_data()
+
+
+def convert_to_excel(response):
+    titlePresent, linesPerFile, sepChar = validateOpts(response)
+    workbook, worksheet = openExcelSheet()
+    fno = 0
+    lno = 0
+    titleCols = []
+    content = response.content.split('\n')
+    reader = csv.reader(content)
+    for line in reader:
+        if (lno == 0 and titlePresent):
+            if (len(titleCols) == 0):
+                titleCols = line
+            writeExcelHeader(worksheet, titleCols)
+        else:
+            writeExcelRow(worksheet, lno, line)
+        lno = lno + 1
+        if (linesPerFile != -1 and lno >= linesPerFile):
+            fno = fno + 1
+            lno = 0
+    closeExcelSheet(response, workbook)
