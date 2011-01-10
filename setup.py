297c9d2423016d1d89fa0d85a30337172db4422a
pmartin <pmartin@43d3a112-9a52-0410-bc29-92ccca371478>
2011-01-10 16:42:57 +0000

    See #129 Adding setup

---
+++ setup.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2010 by Yaco Sistemas <pmartin@yaco.es>
+#
+# This software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import re
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
+
+
+def parse_dependency_links(file_name):
+    dependency_links = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'\s*-[ef]\s+', line):
+            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
+
+    return dependency_links
+
+
+data_files = []
+for dirpath, dirnames, filenames in os.walk('.'):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        continue
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+
+setup(
+    name="django-autoreports",
+    version="0.5",
+    install_requires = parse_requirements('requirements.txt'),
+    dependency_links = parse_dependency_links('requirements.txt'),
+    author="Yaco Sistemas S.L.",
+    author_email="pmartin@yaco.es",
+    description="Django application that lets you create reports",
+    long_description=(read('README')),
+    url='http://tracpub.yaco.es/djangoapps/wiki/AutoReports',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    ],
+    license="LGPL 3",
+    keywords="django autreports reports csv xls excel",
+    packages=find_packages('.'),
+    include_package_data=True,
+)
