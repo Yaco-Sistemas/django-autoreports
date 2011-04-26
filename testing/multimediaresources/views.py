9081ae91d1984e7a76e80adbec70152865b6f51f
pmartin <pmartin@43d3a112-9a52-0410-bc29-92ccca371478>
2011-04-26 16:15:22 +0000

    Fixes #203 Now work the help text

--- testing/multimediaresources/views.py
+++ testing/multimediaresources/views.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-from multimediaresources.models import Resource, TypeResource
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 
 
 def index(request):
-    return render_to_response('multimediaresources/index.html',
-                              {'resources': Resource.objects.all(),
-                               'types': TypeResource.objects.all()},
-                              context_instance=RequestContext(request))
+    admins = User.objects.filter(is_superuser=True)
+    if admins:
+        user = admins[0]
+        user.backend = settings.AUTHENTICATION_BACKENDS[0]
+        login(request, user)
+    return HttpResponseRedirect('/admin/multimediaresources/resource/')
